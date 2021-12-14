f620e2412a8298d87cf30cb03b6c926051e3fa30
Kathy Simon <kathysimon99@gmail.com>
2021-12-14 18:58:14 -0500

    fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,9 +54,9 @@ if page == 'Make a Prediction':
         with st.spinner("Classifying the audio command..."):
             input_len = 16000
             waveform = waveform[:input_len]
-            st.write(tf.shape(waveform))
             zero_padding = tf.zeros([16000] - tf.shape(waveform),
                 dtype=tf.float32)
+            st.write(tf.shape(waveform), zero_padding)
             waveform = tf.cast(waveform, dtype=tf.float32)            
             equal_length = tf.concat([waveform, zero_padding], 0)
             spectrogram = tf.signal.stft(equal_length, frame_length=255, frame_step=128)
