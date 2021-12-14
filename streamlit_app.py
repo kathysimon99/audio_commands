b27e518ecbfb2bfaef2a7036797d263e7be6b36d
Kathy Simon <kathysimon99@gmail.com>
2021-12-14 14:33:47 -0500

    fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,8 +31,47 @@ if page == 'Make a Prediction':
     
     model = load_model('best_cnn.h5')
 
-    new_file = st.file_uploader('Update .wav file here', type = 'wav')
-    new_audio, _ = tf.audio.decode_wav(new_file)
+    def upload_and_save_wavfiles(save_dir: str) -> List[Path]:
+    """ limited 200MB, you could increase by `streamlit run foo.py --server.maxUploadSize=1024` """
+        uploaded_files = st.file_uploader("upload", type=['wav', 'mp3'] accept_multiple_files=True)
+        save_paths = []
+        for uploaded_file in uploaded_files:
+            if uploaded_file is not None:
+                if uploaded_file.name.endswith('wav'):
+                    audio = pydub.AudioSegment.from_wav(uploaded_file)
+                    file_type = 'wav'
+                elif uploaded_file.name.endswith('mp3'):
+                    audio = pydub.AudioSegment.from_mp3(uploaded_file)
+                    file_type = 'mp3'
+
+                save_path = Path(save_dir) / uploaded_file.name
+                save_paths.append(save_path)
+                audio.export(save_path, format=file_type)
+        return save_paths
+
+    def display_wavfile(wavpath):
+        audio_bytes = open(wavpath, 'rb').read()
+        file_type = Path(wavpath).suffix
+        st.audio(audio_bytes, format=f'audio/{file_type}', start_time=0)
+
+
+    files = upload_and_save_wavfiles('temp')
+
+    for wavpath in files:
+        display_wavfile(wavpath)
+
+
+
+
+
+
+
+
+
+
+
+    #new_file = st.file_uploader('Update .wav file here', type = 'wav')
+    #new_audio, _ = tf.audio.decode_wav(new_file)
     #if st.button("Record"):
     #   record_state = st.text("Recording...")
     #    duration = 1  # seconds
