8e9922f896f9b9bc18da465ae89d9961f77d1dfd
Kathy Simon <kathysimon99@gmail.com>
2021-12-14 14:44:34 -0500

    fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,7 @@ from tensorflow.keras.models import load_model
 #from tensorflow.keras import layers
 #from tensorflow.keras import models
 #import soundfile as sf
+from scipy.io import wavfile as wav
 import os
 import librosa
 import glob
@@ -32,46 +33,9 @@ if page == 'Make a Prediction':
     
     model = load_model('best_cnn.h5')
 
-    def upload_and_save_wavfiles(save_dir: str) -> List[Path]:
-        """ limited 200MB, you could increase by `streamlit run foo.py --server.maxUploadSize=1024` """
-        uploaded_files = st.file_uploader("upload", type=['wav', 'mp3'], accept_multiple_files=True)
-        save_paths = []
-        for uploaded_file in uploaded_files:
-            if uploaded_file is not None:
-                if uploaded_file.name.endswith('wav'):
-                    audio = pydub.AudioSegment.from_wav(uploaded_file)
-                    file_type = 'wav'
-                elif uploaded_file.name.endswith('mp3'):
-                    audio = pydub.AudioSegment.from_mp3(uploaded_file)
-                    file_type = 'mp3'
+    new_file = st.file_uploader('Update .wav file here', type = 'wav')
+    sampling_rate, new_audio =  wav.read(new_file)
 
-                save_path = Path(save_dir) / uploaded_file.name
-                save_paths.append(save_path)
-                audio.export(save_path, format=file_type)
-        return save_paths
-
-    def display_wavfile(wavpath):
-        audio_bytes = open(wavpath, 'rb').read()
-        file_type = Path(wavpath).suffix
-        st.audio(audio_bytes, format=f'audio/{file_type}', start_time=0)
-
-
-    files = upload_and_save_wavfiles('temp')
-
-    for wavpath in files:
-        display_wavfile(wavpath)
-
-
-
-
-
-
-
-
-
-
-
-    #new_file = st.file_uploader('Update .wav file here', type = 'wav')
     #new_audio, _ = tf.audio.decode_wav(new_file)
     #if st.button("Record"):
     #   record_state = st.text("Recording...")
